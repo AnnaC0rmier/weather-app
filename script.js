79569edae3b6d3f8fe7fdd3e4f99c1017290470d
AnnaC0rmier <anna.cormier.96@gmail.com>
2024-01-09 03:12:28 -0600

    made changes to JS functions and css styles

--- script.js
+++ script.js
@@ -2,24 +2,54 @@
     // const apiKey = '566ee9a9bfbe514d116ccbba91821eba'
     // const url = 'https://api.openweathermap.org/data/2.5/weather?q=Austin&metrics=imperial';
 
+    const url = 'https://api.openweathermap.org/data/2.5/forecast?q=Austin&units=imperial&appid=566ee9a9bfbe514d116ccbba91821eba';
+
+
+    const input = $('input')
+
+  const getUserInput = function(event) {
+    event.preventDefault();
+
+    console.log()
+  }
     
+
+
+
     function getWeatherData() {
-        var url = 'https://api.openweathermap.org/data/2.5/forecast?q=Austin&units=imperial&appid=566ee9a9bfbe514d116ccbba91821eba';
     
         fetch(url)
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
+        const city = data.city.name
+    
+        let extractedIndex = [0, 8, 16, 24, 32, 39];
+        let newDataObject = extractedIndex.map(index => data.list[index]);
 
-                var forecastEntry = data.list[0];
-                document.querySelector('.date').innerHTML = 'Date: ' + forecastEntry.dt_txt;
-                document.querySelector('.city').innerHTML = data.city.name;
-                document.querySelector('.temp').innerHTML = 'Temperature: ' + forecastEntry.main.temp + 'F';
-                document.querySelector('.humidity').innerHTML = 'Humidity: ' + forecastEntry.main.humidity + '%';
-                document.querySelector('.wind').innerHTML = 'Wind speed: ' + forecastEntry.wind.speed + ' MPH';
+        const date = $('.date');
+        const temp = $('.temp');
+        const wind = $('.wind');
+        const humidity = $('.humidity');
+        const icon = $('.icon')
 
+        newDataObject.forEach((list, index) => {
+            date.eq(index).text ('Date: ' + list.dt_txt);
+            temp.eq(index).text('Temp: ' + list.main.temp + ' Deg F');
+            wind.eq(index).text('Wind Speed: ' + list.wind.speed + ' MPH');
+            humidity.eq(index).text('Humidity: ' + list.main.humidity + '%');
+            icon.eq(index).text(list.weather[0].icon);
+
+            console.log(list.weather[0].icon);
+
+          });  
             })
-            };
-    
-    
+            };  
+            
+            
     getWeatherData();
+
+
+    document.getElementById('search').addEventListener('submit', getUserInput)
+
+
