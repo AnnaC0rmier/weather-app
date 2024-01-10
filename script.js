ced395d188a54e977e0e7cdcbbe3a914d342c2af
AnnaC0rmier <anna.cormier.96@gmail.com>
2024-01-09 23:01:23 -0600

    added eventlistner to handle user input

--- script.js
+++ script.js
@@ -1,55 +1,61 @@
 
-    // const apiKey = '566ee9a9bfbe514d116ccbba91821eba'
-    // const url = 'https://api.openweathermap.org/data/2.5/weather?q=Austin&metrics=imperial';
-
-    const url = 'https://api.openweathermap.org/data/2.5/forecast?q=Austin&units=imperial&appid=566ee9a9bfbe514d116ccbba91821eba';
-
-
-    const input = $('input')
-
-  const getUserInput = function(event) {
-    event.preventDefault();
-
-    console.log()
-  }
-    
+    const form = document.querySelector('form')
+    const input = document.querySelector('#searchBar')
 
 
 
-    function getWeatherData() {
-    
+    function getWeatherData(city) {
+        const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=566ee9a9bfbe514d116ccbba91821eba`;
+
+       
         fetch(url)
             .then((response) => response.json())
             .then((data) => {
-                console.log(data);
-        const city = data.city.name
-    
+        
+             document.querySelector('.city').innerHTML = data.city.name;
+        
+            
         let extractedIndex = [0, 8, 16, 24, 32, 39];
         let newDataObject = extractedIndex.map(index => data.list[index]);
-
+        
         const date = $('.date');
         const temp = $('.temp');
         const wind = $('.wind');
         const humidity = $('.humidity');
-        const icon = $('.icon')
+        
 
-        newDataObject.forEach((list, index) => {
+        
+
+        newDataObject.forEach(( list, index) => {
             date.eq(index).text ('Date: ' + list.dt_txt);
             temp.eq(index).text('Temp: ' + list.main.temp + ' Deg F');
             wind.eq(index).text('Wind Speed: ' + list.wind.speed + ' MPH');
             humidity.eq(index).text('Humidity: ' + list.main.humidity + '%');
-            icon.eq(index).text(list.weather[0].icon);
-
-            console.log(list.weather[0].icon);
 
           });  
             })
             };  
+
+
+
+        const getUserInput = (event) => {
+
+            event.preventDefault();
+            
+            const userInput = input.value
             
             
-    getWeatherData();
+            if (userInput !== ''){
+                 console.log(userInput);
 
+                //  <button type="button" class="btn btn-light"></button>
+            
+                 getWeatherData(userInput);
+                }
+            }
+            
+            
+    // getWeatherData();
 
-    document.getElementById('search').addEventListener('submit', getUserInput)
-
-
+  
+form.addEventListener('submit', getUserInput)
