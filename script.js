cff5e6c5f1cda7a5ecacaae53d1463d072cac70d
AnnaC0rmier <anna.cormier.96@gmail.com>
2024-01-10 21:41:53 -0600

    removed button function.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const form = document.querySelector('form')
 const input = document.querySelector('#searchBar')
-const savedCityArray = [];
+
+
+
 
 function getWeatherData(city) {
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=566ee9a9bfbe514d116ccbba91821eba`;
-
 
     fetch(url)
         .then((response) => response.json())
@@ -27,37 +28,19 @@ function getWeatherData(city) {
                 temp.eq(index).text('Temp: ' + list.main.temp + ' Deg F');
                 wind.eq(index).text('Wind Speed: ' + list.wind.speed + ' MPH');
                 humidity.eq(index).text('Humidity: ' + list.main.humidity + '%');
-
-            });
+            });  
+           
         })
 };
+
 
 const getUserInput = (event) => {
     event.preventDefault();
     const userInput = input.value
     if (userInput !== '') {
         localStorage.setItem('city', userInput);
-        savedCityArray.push(userInput);
-        console.log(savedCityArray);
         getWeatherData(userInput);
-
     }
 }
-
-
-
-const createButton = () => {
-    const buttonContainer = document.getElementById('search-history');
-
-    for (let i = 0; i < savedCityArray.length; i++) {
-        const button = document.createElement('button');
-        button.classList.add('btn', 'btn-light');
-        button.textContent = savedCityArray[i];
-        buttonContainer.appendChild(button);
-    }
-}
-
-
-form.addEventListener('submit', createButton);
 
 form.addEventListener('submit', getUserInput);
